1152113af6052641eebb67757180c33b48eaebea
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-11-29 21:55:29 -0800

    SPARK-5 added `__init__.py` package interface for Parametrize

---
+++ Modules/parametrize/__init__.py
@@ -0,0 +1,24 @@
+"""
+__name__.py - Parametrize namespace
+
+Namespace for all things Parametrize.
+
+Includes type definitions and the decorator itself.
+
+Copyright (c) 2018 The Fuel Rats Mischief,
+All rights reserved.
+
+Licensed under the BSD 3-Clause License.
+
+See LICENSE.md
+"""
+from .types import Rescue, Rat, Name, Index
+from .parametrize import parametrize
+
+__all__ = [
+    "Rescue",
+    "Rat",
+    "Name",
+    "Index",
+    "parametrize"
+]
