0c2f4f3584c2b9890a0f854b1190329f317b671d
theunkn0wn1 <thHunkn0WNd@gmail.com>
2018-11-29 20:04:07 -0800

    SPARK-5 Added Rescue and Rat generic types

--- Modules/parametrize/types.py
+++ Modules/parametrize/types.py
@@ -1,24 +1,54 @@
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, TYPE_CHECKING, Union
+from uuid import UUID
 
 from Modules.rat_rescue import Rescue as _Rescue
 
-RT = TypeVar('RT', _Rescue, int, str)  # Rescue type
+from Modules.rat import Rat as _Rat
+if TYPE_CHECKING:
+    # noinspection PyUnresolvedReferences
+    from utils.ratlib import Platforms as _Platforms
 
 
-class Rescue(Generic[RT]):
-    def __repr__(self):
-        return "types.Rescue"
+# typedefs to builtin types (OK For export)
+Index = int
+Name = str
+
+# typeVars (OK For Export)
+RescueType = TypeVar('RescueType', int, Name, UUID)  # Rescue type
+RatType = TypeVar("RatType", '_Platforms', str, UUID)  # rat type
 
 
-def foo(bar: Rescue[str]):
-    print(bar)
+class Rescue(Generic[RescueType], _Rescue):
+    """
+    Rescue type parameter, for use with @parametrize
+
+    Functions using this hint will have an argument block added to it to find a Rescue from the
+    context by the parametrize decorator
+
+    Basic usage:
+        >>> def foo(rescue:Rescue):
+        ...     ...
+
+    Parametrize will create a positional argument group for the argument.
+    This group accepts a String commander name, a integer case index, or a UUID
+
+    Rescues can also be parametrized by their index:
+        >>> def foo(rescue:Rescue[Index]):
+        ...     ...
+
+        Parametrize will register a positional integer argument for `rescue_a` that will resolve
+        to a Rescue on the Context.bot.board object by its index.
 
 
-mdata = _Rescue(None, None, None, None)
+    Alternatively, they can be parametrized by their api ID:
+        >>> def foo(rescue:Rescue[UUID]):
+        ...     ...
 
+    Please note that the optional filtering types are mutually exclusive, and cannot be mixed.
 
-def bar() -> Rescue[int]:
+    """
     ...
 
 
-foo(bar())
+class Rat(Generic[RatType], _Rat):
+    ...
