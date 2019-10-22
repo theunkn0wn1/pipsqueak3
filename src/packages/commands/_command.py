6379acc8ea6949c7c5ca8b1c19a8f24118c5e5e5
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-10-21 21:08:18 -0700

    tie in pre-processor hooks and underlying invocation

--- src/packages/commands/_command.py
+++ src/packages/commands/_command.py
@@ -13,15 +13,13 @@ This module is built on top of the Pydle system.
 """
 from __future__ import annotations
 
-import logging
 import typing
 from dataclasses import dataclass, field
 
 from ._exceptions import Abort, UsageError
 from ..context import Context
+from loguru import logger
 
-# set the logger for rat_command
-LOG = logging.getLogger(f"mecha.{__name__}")
 _CALLABLE_TYPE = "typing.Callable[[Context, ...], typing.Any]"
 
 
@@ -31,11 +29,26 @@ class Command:
     usage: str = ""
     pre_execution_hooks: typing.List[_CALLABLE_TYPE] = field(default_factory=list)
 
-    async def __call__(self, ctx: Context, *args, **kwargs):
-        ...
+    async def __call__(self, ctx: Context):
+        logger.trace("entering command __call__")
+        extra_args: typing.Dict[str, typing.Any] = {}
+
         result = None
+
+        logger.trace("executing pre-execution hooks...")
         try:
-            result = await self.underlying(ctx=ctx, *args, **kwargs)
+            # invoke pre-execution hooks, anything returned by hooks
+            # update the kwarg dict to call the underlying with
+            for hook in self.pre_execution_hooks:
+                logger.trace("executing pre-command hook {}", hook)
+                result = await hook(ctx)
+                logger.trace("done with hook {}", hook)
+                logger.debug("pre-execution hook {} resulted in {}", hook, result)
+                if result:
+                    extra_args.update(result)
+
+            logger.debug("calling underlying with extra_args:= {}", extra_args)
+            await self.underlying(ctx, **extra_args)
         except UsageError:
             # usage error
             await ctx.reply(self.usage)
@@ -44,10 +57,3 @@ class Command:
 
         finally:
             return result
-
-
-async def uhh(ctx: Context, potaot: int):
-    ...
-
-
-# command = Command( underlying=uhh)
