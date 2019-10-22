904eb614d58e8433001309ac296e56065abfdbf4
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-10-21 22:06:21 -0700

    fix tests to reflect new `Command` decorator behavior

--- tests/unit/commands/test_Command.py
+++ tests/unit/commands/test_Command.py
@@ -4,20 +4,22 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_abort(bot_fx, context_fx, async_callable_fx):
-    cmd = Command(underlying=async_callable_fx, usage="usage text")
+async def test_abort(bot_fx, context_fx, async_callable_fx, random_string_fx):
+    cmd = Command(f"cmd_{random_string_fx}", usage="usage text")
+    cmd.underlying = async_callable_fx
     async_callable_fx.exception_to_raise = Abort
-    await cmd(context_fx)
+    await cmd.invoke(context_fx)
 
     assert async_callable_fx.was_called
     assert "aborted" in bot_fx.sent_messages[0]['message'].casefold()
 
 
 @pytest.mark.asyncio
-async def test_usage_error(bot_fx, context_fx, async_callable_fx):
-    cmd = Command(underlying=async_callable_fx, usage="usage text")
+async def test_usage_error(bot_fx, context_fx, async_callable_fx, random_string_fx):
+    cmd = Command(f"cmd_{random_string_fx}", usage="usage text")
+    cmd.underlying = async_callable_fx
     async_callable_fx.exception_to_raise = UsageError
-    await cmd(context_fx)
+    await cmd.invoke(context_fx)
 
     assert async_callable_fx.was_called
     assert "usage" in bot_fx.sent_messages[0]['message'].casefold()
